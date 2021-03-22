6f2a43638338c745acbb0ee48fc31a90bec843bf
Ben Greenier <ben@bengreenier.com>
2021-03-21 19:36:40 -0700

    feat(cd): Add build config 📝 (#6)
    * feat(cd): Attempt build config
    
    * fix(cd): unquote echo
    
    * fix(cd): requote echo
    
    * fix(cd): single quote idk
    
    * fix(cd): remove unused values

--- packages/tm.az-visuals.com/src/App.tsx
+++ packages/tm.az-visuals.com/src/App.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import { AuthWrapper } from "./Auth";
 import "./App.css";
 import { Visual } from "./Visual";
-
+import { clientId, redirectUri } from "./build-config.json";
 /**
  * Default react App component - React "Entrypoint"
  * @returns React component
  */
 function App() {
   return (
-    <AuthWrapper
-      clientId="56b74d6c-2ee9-4384-adae-06c968b6ee1b"
-      redirectUri="http://localhost:3000"
-    >
+    <AuthWrapper clientId={clientId} redirectUri={redirectUri}>
       <div className="App">
         <Visual />
       </div>
